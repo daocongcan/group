c2ae9d76278e75f45389fe1a0596188f7709f5fa
candc <candc@dfm-engineering.com>
2018-08-02 14:28:00 +0700

    edit pagination

--- plugins/filipac/facebookmessengerpopup/lang/en/lang.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'plugin' => [
-        'name' => 'Facebook Messenger popup',
-        'description' => 'Allow your visitors to live chat with your business using Facebook Messenger.',
-    ],
-];
